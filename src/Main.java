fb277be1fcfbd4b6441887181a862fb1388a102a
tatsuya828828 <kkjy011894@gmail.com>
2020-07-08 17:20:16 +0900

    [finish]11-2

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,23 @@
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.URL;
+import java.io.OutputStream;
+import java.net.Socket;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
-		URL url = new URL("http://dokojava.jp");
-		InputStream is = url.openStream();
+		Socket sock = new Socket("dokojava.jp", 80);
+		InputStream is = sock.getInputStream();
+		OutputStream os = sock.getOutputStream();
+		os.write("GET /index.html HTTP/1.0¥r¥n".getBytes());
+		os.write("¥r¥n".getBytes());
+		os.flush();
 		InputStreamReader isr = new InputStreamReader(is);
 		int i = isr.read();
 		while(i != -1) {
 			System.out.print((char) i);
 			i = isr.read();
 		}
-		isr.close();
+		sock.close();
 	}
 }
